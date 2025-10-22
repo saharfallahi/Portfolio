b4edb80c7c910cdbe93c46c2b2ab3feec882704f
saharF <s.fallahi.66@gmail.com>
2025-10-22 13:46:57 +0330

    add sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://sahar-fallahi.ir',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 7000,
+  outDir:"public"
+}
